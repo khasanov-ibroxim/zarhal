14fef452f305d340efb74d9e0a1a0cdbeadfebca
khasanov-ibroxim <xibroxim11@gmail.com>
2025-06-22 14:06:50 +0500

    lk

--- src/page/contact/contact.jsx
+++ src/page/contact/contact.jsx
@@ -17,24 +17,23 @@ const Contact = () => {
                 <div className="contact_content">
                     <div className="contact_form">
                         <div className="contact_title">
-                            <h1>Contact Form</h1>
-                            <p>Feel free to contact us through <a href="#">Twitter</a> or <a href="#">Facebook</a> if
-                                you prefer.</p>
+                            <h1>Свяжитесь с нами</h1>
+                            <p>Оставьте заявку — мы свяжемся с вами</p>
                         </div>
                         <div className="contact_box d-flex justify-content-center align-items-center flex-column ">
                             <div className="input_box">
-                                <input type="text" placeholder={"Your Name"}/>
-                                <input type="text" placeholder={"Your Email"}/>
-                                <input type="text" placeholder={"Phone Number"}/>
+                                <input type="text" placeholder={"Имя "}/>
+                                <input type="text" placeholder={"Email"}/>
+                                <input type="text" placeholder={"Телефон"}/>
                             </div>
                             <div className="input_box mt-4">
-                                <input type="text" placeholder={"Company name"}/>
-                                <input type="text" placeholder={"Subject"}/>
+                                <input type="text" placeholder={"Компания"}/>
+
                             </div>
                             <div className="input_box mt-4">
-                                <textarea rows={7} placeholder={"Message"}/>
+                                <textarea rows={7} placeholder={"Сообщение"}/>
                             </div>
-                            <button className={"contact_btn"}>Send Now</button>
+                            <button className={"contact_btn"}>Отправить заявку</button>
                         </div>
                     </div>
 
@@ -42,7 +41,7 @@ const Contact = () => {
                         <div className="row">
                             <div className="col-lg-5">
                                 <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10093.221062694342!2d69.22499776479745!3d41.227597899959214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae61a2ca2658d9%3A0xea6f4e90abf3d47d!2sSergeli%20Dehqon%20Bozori!5e0!3m2!1sru!2s!4v1750357741134!5m2!1sru!2s"
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2579.6086086002697!2d64.45117705644078!3d39.74663827859795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f500527334645c9%3A0x187c4ea907cfe176!2sZarhal%20Group!5e0!3m2!1sru!2s!4v1750583046704!5m2!1sru!2s"
                                     width="100%" height="100%" allowFullScreen="" loading="lazy"
                                     className={"map"}
                                     referrerPolicy="no-referrer-when-downgrade"></iframe>
@@ -50,22 +49,22 @@ const Contact = () => {
                             <div className="col-lg-7">
                                 <div className="contact_info_box">
                                     <div className="contact_info_title">
-                                        <div className="subtitle">Contact us</div>
-                                        <h1>Get In Touch!</h1>
-                                        <p>Loream ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor
-                                            incididunt ut labore et dolore magna aliqua.</p>
+                                        <div className="subtitle">Контакты</div>
+                                        <h1>Как нас найти?</h1>
+                                        <p>Наше производство удобно расположено с хорошей транспортной доступностью. Мы
+                                            с радостью проведём экскурсию по цехам и покажем, как создаётся качественная
+                                            трикотажная продукция. Предварительная запись приветствуется.</p>
                                     </div>
                                     <div className="contact_info_box_content">
-                                        <h2>Head Office</h2>
+                                        <h2>Главный офис</h2>
                                         <div className="contact_info_item_box">
                                             <div className="contact_info_item">
                                                 <div className="contact_info_item_left">
                                                     <LocationOnOutlinedIcon/>
                                                 </div>
                                                 <div className="contact_info_item_right">
-                                                    <h4>Address</h4>
-                                                    <p>123 King Street,Melbourne Victoria 5000,New York.</p>
+                                                    <h4>Адрес</h4>
+                                                    <p>Республика Узбекистан, г.Бухара, улица Алпамыша 331</p>
                                                 </div>
                                             </div>
                                             <div className="contact_info_item">
@@ -73,31 +72,9 @@ const Contact = () => {
                                                     <EmailOutlinedIcon/>
                                                 </div>
                                                 <div className="contact_info_item_right">
-                                                    <h4>Call Us / Email</h4>
-                                                    <p>+1800-200-123456</p>
-                                                    <p>fablio.support@yourmail.com</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <h2>Branch Office</h2>
-                                        <div className="contact_info_item_box">
-                                            <div className="contact_info_item">
-                                                <div className="contact_info_item_left">
-                                                    <LocationOnOutlinedIcon/>
-                                                </div>
-                                                <div className="contact_info_item_right">
-                                                    <h4>Address</h4>
-                                                    <p>123 King Street,Melbourne Victoria 5000,New York.</p>
-                                                </div>
-                                            </div>
-                                            <div className="contact_info_item">
-                                                <div className="contact_info_item_left">
-                                                    <EmailOutlinedIcon/>
-                                                </div>
-                                                <div className="contact_info_item_right">
-                                                    <h4>Call Us / Email</h4>
-                                                    <p>+1800-200-123456</p>
-                                                    <p>fablio.support@yourmail.com</p>
+                                                    <h4>Телефон/Email</h4>
+                                                    <p>+998652220707</p>
+                                                    <p>zarhalgroup.uz@gmail.com</p>
                                                 </div>
                                             </div>
                                         </div>
