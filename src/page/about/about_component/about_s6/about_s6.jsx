b277698e474b8c7402971747bb0c68fb9c98007c
khasanov_ibroxim <xibroxim11@gmail.com>
2025-09-18 15:52:03 +0500

    11

--- src/page/about/about_component/about_s6/about_s6.jsx
+++ src/page/about/about_component/about_s6/about_s6.jsx
@@ -5,14 +5,14 @@ import img_1 from "@/assets/about/about_s6/DSC03779.jpg"
 import img_2 from "@/assets/about/about_s6/DSC03060.jpg"
 import img_3 from "@/assets/about/about_s6/DSC033352.jpg"
 
-import l1 from "@/assets/about/about_s6/brand/bosso.png"
-import l2 from "@/assets/about/about_s6/brand/bzt.png"
-import l3 from "@/assets/about/about_s6/brand/bodimax.png"
-import l4 from "@/assets/about/about_s6/brand/ksht.png"
-import l5 from "@/assets/about/about_s6/brand/langheinrich.png"
-import l6 from "@/assets/about/about_s6/brand/oc.png"
-import l7 from "@/assets/about/about_s6/brand/GS.png"
-import l8 from "@/assets/about/about_s6/brand/sp.png"
+import l1 from "@/assets/about/about_s6/brand/bzt.png"
+import l2 from "@/assets/about/about_s6/brand/bodimax.png"
+import l3 from "@/assets/about/about_s6/brand/ksht.png"
+import l4 from "@/assets/about/about_s6/brand/oc.png"
+import l5 from "@/assets/about/about_s6/brand/sp.png"
+import l6 from "@/assets/about/about_s6/brand/langheinrich.png"
+import l7 from "@/assets/about/about_s6/brand/bosso.png"
+import l8 from "@/assets/about/about_s6/brand/GS.png"
 
 
 import {Autoplay} from "swiper/modules";
