0bef0ad96802ce98e3a230dc4cebd2f7ea9c5979
khasanov_ibroxim <xibroxim11@gmail.com>
2025-09-25 23:30:29 +0500

    11

--- src/utils/lang/translates/blog_translate.jsx
+++ src/utils/lang/translates/blog_translate.jsx
@@ -1,8 +1,34 @@
 export const blog_translate = {
     ru:{
-        page_title:"Блог"
+        page_title:"Блог",
+        i1:{
+            title:`В Узбекистане впервые приобретён глобальный текстильный бренд`,
+            full_desc:`Компания «Kamalak Shabnam Teks» полностью приобрела известную во всём мире немецкую текстильную компанию «Curt Bauer», имеющую более 140 летную историю, а также перевела в Узбекистан все активы бренда, технологии и права интеллектуальной собственности.
+Это предприятие, расположенное в Бухарской области, специализируется на производстве спецодежды,одежды и постельное бельё для различных отраслей. В рамках нового проекта планируется производство современных текстильных материалов на основе полиэстера и полиамида для автомобилестроения, авиации, обороны, медицины и бытовой промышленности.
+Данный проект не только создаст 785 новых рабочих мест, но и привлечёт передовые технологии Германии в нашу страну.
+Проект способствует диверсификации промышленности Узбекистана, увеличению экспортного потенциала и привлечению прямых иностранных инвестиций.`
+        },
+        i2:{
+            title:`Мы почти завершили подготовку полей для выращивания органического хлопка`,
+            full_desc:`
+        Мы почти завершили подготовку полей для выращивания органического хлопка — и уже с 2026 года начнем выпуск продукции, изготовленной органического сырья.
+        Это не просто новый этап, а наша инвестиция в чистое будущее и экологически ответственное производство.`
+        },
     },
     en:{
-        page_title:"Blog"
+        page_title:"Blog",
+        i1:{
+            title:`A global textile brand has been acquired in Uzbekistan for the first time.`,
+            full_desc:`The company “Kamalak Shabnam Teks” has fully acquired the world-renowned German textile company “Curt Bauer”, with a history of more than 140 years, and has transferred all the brand’s assets, technologies, and intellectual property rights to Uzbekistan.
+
+This enterprise, located in the Bukhara region, specializes in the production of workwear, clothing, and bed linen for various industries. As part of the new project, it is planned to manufacture advanced textile materials based on polyester and polyamide for the automotive, aviation, defense, medical, and household industries.
+
+The project will not only create 785 new jobs, but will also bring cutting-edge German technologies to our country.
+It contributes to the diversification of Uzbekistan’s industry, the growth of export potential, and the attraction of foreign direct investment.`
+        },
+        i2:{
+            title:`We are almost finished preparing the fields for growing organic cotton `,
+            full_desc:`We are almost finished preparing the fields for growing organic cotton — and starting from 2026, we will begin producing goods made from organic raw materials. This is not just a new stage, but our investment in a clean future and environmentally responsible production.`
+        },
     }
 }
